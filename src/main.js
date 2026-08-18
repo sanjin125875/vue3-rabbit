ffabe600a54f6f9fc191a95df22e60aac8bf9dcf
yangzhixin <3357496451@qq.com>
2026-08-18 19:39:38 +0800

    静态资源以及初始化样式资源

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// 引入初始化的样式文件
+import '@/styles/common.scss'
+
 // 1. 创建应用实例
 const app = createApp(App)
 
