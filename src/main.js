5691759a88c2fc927e370dedb5d42bc7dafdb9ee
yangzhixin <3357496451@qq.com>
2026-08-19 21:36:59 +0800

    home-图片懒加载实现

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import router from './router'
 import '@/styles/common.scss'
 
 // 1. 创建应用实例
+ 
+
+
 const app = createApp(App)
 
 // 2. 注册插件
@@ -16,3 +19,26 @@ app.use(router)         // 路由后注册（也可以反过来）
 
 // 3. 挂载应用
 app.mount('#app')
+
+// 定义懒加载插件
+import { useIntersectionObserver } from '@vueuse/core'
+
+// 定义全局指令
+ app.directive('img-lazy', {
+      mounted (el, binding) {
+        // el: 指令绑定的那个元素 img
+        // binding: binding.value  指令等于号后面绑定的表达式的值  图片url
+        console.log(el, binding.value)
+        const { stop } = useIntersectionObserver(
+          el,
+          ([{ isIntersecting }]) => {
+            console.log(isIntersecting)
+            if (isIntersecting) {
+              // 进入视口区域
+              el.src = binding.value
+              stop()
+            }
+          },
+        )
+      }
+    })
