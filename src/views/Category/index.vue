483689ee395b3fe83065d162dc615993b78e6fc5
yangzhixin <3357496451@qq.com>
2026-08-20 21:28:33 +0800

    一级分类面包屑导航渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,15 +1,119 @@
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue';
+import axios from 'axios'
+import { getTopCategoryAPI } from '@/apis/category';
+import { useRoute } from 'vue-router';
+import { watch } from 'vue';
+const route = useRoute()  
+const topCategory = ref({})
 
+const getTopCategory = async () =>{
+    const res = await getTopCategoryAPI(route.params.id)
+    topCategory.value = res.result
+}
 
+watch(
+  () => route.params.id,
+  (newId) => {
+    if (newId) {
+        getTopCategory()
+    }
+},
+  { immediate: true }
+  ) 
 </script>
 
 <template>
-  <div class="container">
-    我是Category页面
+  <div class="top-category">
+    <div class="container m-top-20">
+      <!-- 面包屑 -->
+      <div class="bread-container">
+        <el-breadcrumb separator=">">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item >{{topCategory.name}}</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
 
+<style scoped lang="scss">
+.top-category {
+  h3 {
+    font-size: 28px;
+    color: #666;
+    font-weight: normal;
+    text-align: center;
+    line-height: 100px;
+  }
+
+  .sub-list {
+    margin-top: 20px;
+    background-color: #fff;
+
+    ul {
+      display: flex;
+      padding: 0 32px;
+      flex-wrap: wrap;
+
+      li {
+        width: 168px;
+        height: 160px;
+
+
+        a {
+          text-align: center;
+          display: block;
+          font-size: 16px;
+
+          img {
+            width: 100px;
+            height: 100px;
+          }
+
+          p {
+            line-height: 40px;
+          }
+
+          &:hover {
+            color: $xtxColor;
+          }
+        }
+      }
+    }
+  }
+
+  .ref-goods {
+    background-color: #fff;
+    margin-top: 20px;
+    position: relative;
+
+    .head {
+      .xtx-more {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+      }
+
+      .tag {
+        text-align: center;
+        color: #999;
+        font-size: 20px;
+        position: relative;
+        top: -20px;
+      }
+    }
+
+    .body {
+      display: flex;
+      justify-content: space-around;
+      padding: 0 40px 30px;
+    }
+  }
+
+  .bread-container {
+    padding: 25px 0;
+  }
+}
 </style>
