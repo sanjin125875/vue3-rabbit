722fc24e9fe3b3e5ddc93a606847de402ef21944
yangzhixin <3357496451@qq.com>
2026-08-23 09:42:20 +0800

    二级分类-tab切换

--- src/views/subCategory/index.vue
+++ src/views/subCategory/index.vue
@@ -24,6 +24,12 @@ const getGoodList = async () => {
 }
   
 onMounted(() => getGoodList())
+
+// tab 切换
+const tabChange= () => {
+  reqData.value.page = 1
+  getGoodList()
+}
 </script>
 
 <template>
@@ -38,7 +44,7 @@ onMounted(() => getGoodList())
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
