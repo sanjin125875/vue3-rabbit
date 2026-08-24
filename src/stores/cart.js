72d92fee988aba7ae1bd541a376b074765f01d94
yangzhixin <3357496451@qq.com>
2026-08-25 07:40:52 +0800

    本地购物车-头部购物车删除

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -17,14 +17,21 @@ export const useCartStore = defineStore(
       }
     };
 
-    const delCart = (id) => {
-        
-    }
+    // 删除购物车
+    const delCart = async (skuId) => {
+      // 思路：
+      // 1. 找到要删除项的下标值 - splice
+      // 2. 使用数组的过滤方法 - filter
+      const idx = cartList.value.findIndex((item) => skuId === item.skuId);
+      cartList.value.splice(idx, 1);
+    };
 
+    //  cartList.value = cartList.value.filter((item) => item.skuId !== skuId);
+    
     return {
       cartList,
       addCart,
-      delCart
+      delCart,
     };
   },
   {
