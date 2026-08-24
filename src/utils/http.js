b024d8b2d5b0c60a90bee2270ab5036a27407d9f
yangzhixin <3357496451@qq.com>
2026-08-24 21:28:02 +0800

    配置token

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@
 import axios from "axios";
 import { ElMessage } from "element-plus";
 import "element-plus/theme-chalk/el-message.css";
+import { useUserStore } from "@/stores/user";
 
 // 创建axios实例
 const httpInstance = axios.create({
@@ -13,6 +14,13 @@ const httpInstance = axios.create({
 // axios请求拦截器
 httpInstance.interceptors.request.use(
   (config) => {
+    // 1. 从pinia获取token数据
+    const userStore = useUserStore();
+    // 2. 按照后端的要求拼接token数据
+    const token = userStore.userInfo.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   (e) => Promise.reject(e),
@@ -25,8 +33,8 @@ httpInstance.interceptors.response.use(
     // 统一错误提示
     ElMessage({
       type: "warning",
-      message: e.response.data.message
-    })
+      message: e.response.data.message,
+    });
     return Promise.reject(e);
   },
 );
