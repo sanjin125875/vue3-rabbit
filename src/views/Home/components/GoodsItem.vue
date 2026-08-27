5f98c39d1b06b126a26c38dc5f9efabdab1969d4
yangzhixin <3357496451@qq.com>
2026-08-27 21:55:03 +0800

    个人信息渲染

--- src/views/Home/components/GoodsItem.vue
+++ src/views/Home/components/GoodsItem.vue
@@ -9,10 +9,10 @@ defineProps({
 
 <template>
   <RouterLink to="/" class="goods-item">
-    <img :src="goods.picture" alt="" />
-    <p class="name ellipsis">{{ goods.name }}</p>
-    <p class="desc ellipsis">{{ goods.desc }}</p>
-    <p class="price">&yen;{{ goods.price }}</p>
+    <img :src="goods?.picture" alt="" />
+    <p class="name ellipsis">{{ goods?.name }}</p>
+    <p class="desc ellipsis">{{ goods?.desc }}</p>
+    <p class="price">&yen;{{ goods?.price }}</p>
   </RouterLink>
 </template>
 
